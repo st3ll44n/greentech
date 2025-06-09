e5f77ce2cd308d5e6c21d54703509bf60bc194b9
Stella Andrade <stellanaziozeno@gmail.com>
2025-06-09 17:45:11 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GreenTech</title>
+    <link rel="stylesheet" href="styles/all.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;600&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<header class="header">
+    <div class="retanguloHeader"></div>
+    <div class="logo"></div>
+
+    <div class="retangulo1"></div>
+    <div class="retangulo2"></div>
+    <div class="retangulo3"></div>
+
+    <h1 class="greentech">GreenTech</h1>
+    <div class="login">LOGIN</div>
+    <div class="home">HOME</div>
+    <div class="catalogo">CATÁLOGO</div>
+  </header>
+
+  <h1>HELLO WORD!</h1>
+    
+
+<footer class="footer">
+    <div class="retanguloFooter"></div>
+    <h4 class="direitos">DIREITOS RESERVADOS A ®GREENTECH</h4>
+</footer>
+
+</body>
+</html>
