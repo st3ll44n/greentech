779d5786befe0bd8c82780303f66880e77b73e2e
Stella Andrade <stellanaziozeno@gmail.com>
2025-06-10 17:29:53 -0300

    Complemento do projeto

--- index.php
+++ index.php
@@ -6,33 +6,90 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GreenTech</title>
     <link rel="stylesheet" href="styles/all.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;600&display=swap" rel="stylesheet">
-</head>
+    <link rel="stylesheet" href="styles/home.css">
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+        </style>    
 <body>
 
-<header class="header">
-    <div class="retanguloHeader"></div>
-    <div class="logo"></div>
+    <header class="header">
+        <div class="retanguloHeader"></div>
+        <div class="logo"></div>
 
-    <div class="retangulo1"></div>
-    <div class="retangulo2"></div>
-    <div class="retangulo3"></div>
+        <div class="retangulo1"></div>
+        <div class="retangulo2"></div>
+        <div class="retangulo3"></div>
 
-    <h1 class="greentech">GreenTech</h1>
-    <div class="login">LOGIN</div>
-    <div class="home">HOME</div>
-    <div class="catalogo">CATÁLOGO</div>
-  </header>
+        <h1 class="greentech">GreenTech</h1>
 
-  <h1>HELLO WORD!</h1>
-    
+        <nav>
+            <a class="home" href="index.php">Home</a>
+            <a class="catalogo" href="pages/catalogo.php">Catálogo</a>
+            <a class="login" href="pages/login.php">Login</a>
+        </nav>
 
-<footer class="footer">
-    <div class="retanguloFooter"></div>
-    <h4 class="direitos">DIREITOS RESERVADOS A ®GREENTECH</h4>
-</footer>
+    </header>
+
+    <br>
+    <main>
+        <div class="Home">Home</div>
+
+        <div style="position: absolute; width: 389px; height: 468px; left: 33px; top: 177px;">
+            <div class="menu1"></div>
+
+        <div class="menu2"></div>
+            <div class="menu menu">MENU</div>
+
+        <div class="menu3"></div>
+            <div class="menuPromo">PROMOÇÃO</div>
+
+        <div class="menu4"></div>
+            <div class="menuSist">SISTEMAS SUSTENTÁVEIS</div>
+
+        <div class="menu5"></div>
+            <div class="menuInsta">INSTALAÇÕES</div>
+
+        <div class="menu6"></div>
+            <div class="menuServiços">SERVIÇOS</div>
+
+        <div class="menu8"></div>
+            <div class="menuSobre">SOBRE NÓS</div>
+
+        <div class="menu9"></div>
+            <div class="menuTrabalhe">TRABALHE CONOSCO</div>
+
+        <div class="menu7"></div>
+            <div class="menuContatos">CONTATOS</div>
+        </div>
+
+
+        <!-- Notícia 1 -->
+        <div class="Container" class="container1" style="top: 269px; left: 440px; width: 430px; height: 180px;">
+            <div class="container2"></div>
+            <p class="noticia">Notícia importante! Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+
+        <!-- Notícia 2 -->
+        <div class="Container2" class="container1" style="top: 469px; left: 440px; width: 430px; height: 180px;">
+            <div class="container2"></div>
+            <p class="noticia">Notícia importante! Donec at leo non justo sollicitudin.</p>
+        </div>
+
+        <!-- Sobre nós - Bloco 1 -->
+        <div class="Container3" class="container3" style="top: 869px; left: 33px; width: 837px; height: 236px;">
+            <div class="container4"></div>
+            <p class="sobrenos">Sobre nós Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+
+        <!-- Imagem
+        <img class="image" src="\images\30e495fd-3e48-42a6-bbb0-e236be7c6244.png" alt=""> -->
+    </main>
+
+    <br>
+    <footer class="fixarRodape" class="footer">
+        <div class="retanguloFooter"></div>
+        <h4 class="direitos">DIREITOS RESERVADOS A ®GREENTECH</h4>
+    </footer>
 
 </body>
 </html>
