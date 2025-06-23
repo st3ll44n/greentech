f10154fde910e2af7851d217b4b108eaee0f6ee5
Stella Andrade <stellanaziozeno@gmail.com>
2025-06-23 17:41:33 -0300

    Complemento do projeto

--- pages/login.php
+++ pages/login.php
@@ -4,32 +4,108 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="imagex/png" href="C:\xampp\htdocs\GreenTech\images\image-3.ico">
+    <link rel="stylesheet" href="C:\xampp\htdocs\GreenTech\styles\all.css">
+    <link rel="stylesheet" href="C:\xampp\htdocs\GreenTech\styles\login.css">
     <title>Login</title>
-    <link rel="stylesheet" href="styles/all.css">
-        <style>
+    
+    <style>
             @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
-        </style>    
+    </style>    
 <body>
 
     <header class="header">
         <div class="retanguloHeader"></div>
-        <div class="logo"></div>
+        <div class="Logo">
+            <img src="C:\xampp\htdocs\GreenTech\images\logo.svg" alt="Logo GreenTech" class="logo" />
+        </div>
 
         <div class="retangulo1"></div>
         <div class="retangulo2"></div>
         <div class="retangulo3"></div>
 
         <h1 class="greentech">GreenTech</h1>
-        <div class="login">LOGIN</div>
-        <div class="home">HOME</div>
-        <div class="catalogo">CATÁLOGO</div>
+
+        <nav>
+        <a class="home" href="C:\xampp\htdocs\GreenTech\index.php">Home</a>
+            <a class="catalogo" href="C:\xampp\htdocs\GreenTech\pages\catalogo.php">Catálogo</a>
+            <a class="login" href="C:\xampp\htdocs\GreenTech\pages\login.php">Login</a>
+        </nav>
+
     </header>
 
     <main>
-        
+        <!-- Título -->
+        <div class="titulo-usuario">Usuário</div>
+
+        <!-- Containers principais -->
+        <div class="container-perfil"></div>
+        <div class="Container1-nome"></div>
+        <div class="Container2 perfil"></div>
+        <div class="Container3 serv"></div>
+
+        <!-- Campos Nome, Email, Telefone -->
+        <div class="container1-nome"></div>
+        <div class="container2-nome"></div>
+        <div class="container3-nome"></div>
+
+        <div class="nome">Nome</div>
+        <div class="joao">João da Silva</div>
+
+        <div class="email">E-mail</div>
+        <div class="endereco-email">joao@email.com</div>
+
+        <div class="telefone">Telefone</div>
+        <div class="numero">(11) 99999-9999</div>
+
+        <!-- Imagem de perfil -->
+        <div class="imagePerfil"></div>
+
+        <!-- Texto explicativo -->
+        <div class="perfil">Perfil</div>
+        <div class="lorem1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</div>
+
+        <div class="servicos">Serviços</div>
+        <div class="lorem2">Curabitur blandit tempus porttitor. Vestibulum id ligula porta felis euismod semper.</div>
+
+        <!-- Menu Lateral -->
+        <div style="position: absolute; width: 389px; height: 468px; left: 33px; top: 177px;">
+            <div class="menu1"></div>
+
+        <div class="menu2"></div>
+            <div class="menu menu">MENU</div>
+
+        <div class="menu3"></div>
+            <div class="menuPromo">PROMOÇÃO</div>
+
+        <div class="menu4"></div>
+            <div class="menuSist">SISTEMAS SUSTENTÁVEIS</div>
+
+        <div class="menu5"></div>
+            <div class="menuInsta">INSTALAÇÕES</div>
+
+        <div class="menu6"></div>
+            <div class="menuServiços">SERVIÇOS</div>
+
+        <div class="menu8"></div>
+            <div class="menuSobre">SOBRE NÓS</div>
+
+        <div class="menu9"></div>
+            <div class="menuTrabalhe">TRABALHE CONOSCO</div>
+
+        <div class="menu7"></div>
+            <div class="menuContatos">CONTATOS</div>
+        </div>
+
+        <!-- Menu de opções inferior -->
+        <div class="menuOpcao">
+        <div class="opcao1">Opção 1</div>
+        <div class="opcao2">Opção 2</div>
+        <div class="opcao3">Opção 3</div>
+        </div>
     </main>
 
-    <footer class="footer">
+    <footer class="fixarRodape" class="footer">
         <div class="retanguloFooter"></div>
         <h4 class="direitos">DIREITOS RESERVADOS A ®GREENTECH</h4>
     </footer>
